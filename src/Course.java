bffe70bb08e4ca7a68e05ef1b8eb23c74b897be2
nickcheng234 <ncheng29@uwo.ca>
2017-02-16 00:36:34 -0500

    Course, student, instructor changes.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Nick on 16/02/2017.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
